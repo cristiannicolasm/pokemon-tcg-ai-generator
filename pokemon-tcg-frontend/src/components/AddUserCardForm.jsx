ae0093faf35f50e7ddb99d47256c5e9889235380
cristiannicolasm <cristiannicolas.m1@gmail.com>
2025-10-05 20:26:34 -0300

    POKEIA-29 Implementación de Pruebas de Integración para Endpoints de Colección.

--- pokemon-tcg-frontend/src/components/AddUserCardForm.jsx
+++ pokemon-tcg-frontend/src/components/AddUserCardForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./AddUserCardForm.css"; // Crea este archivo para los estilos
+import axios from '../axiosInstance';
 
 function AddUserCardForm({ token, onSuccess }) {
   const [form, setForm] = useState({
